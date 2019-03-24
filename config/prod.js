ecbb2b4f2eacbace91bcd38d506964ea874d089a
Jeff Tian <jeff.tian@outlook.com>
2019-03-24 18:02:14 +0800

    add homepage

--- config/prod.js
+++ config/prod.js
@@ -2,8 +2,9 @@ module.exports = {
   env: {
     NODE_ENV: '"production"'
   },
-  defineConstants: {
-  },
+  defineConstants: {},
   weapp: {},
-  h5: {}
+  h5: {
+    publicPath: new URL(require('../package.json').homepage).pathname
+  }
 }
