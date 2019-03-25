ba0a6928f73628c7f9f767fed4c0cb9d1bbc8c09
Jeff Tian <jie.tian@unicareer.com>
2019-03-25 13:00:55 +0800

    recover login

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,23 +1,19 @@
-// import {UserAgentApplication} from "msal";
-//
-// const applicationConfig = {
-//   clientID: 'bacb8d3b-6ee0-4443-9bea-b54485a5a20d',
-//   authority: 'https://login.microsoftonline.com/tfp/unihearti.onmicrosoft.com/B2C_1_tictactoe',
-//   b2cScopes: ["https://unihearti.onmicrosoft.com/hello/demo.read"],
-// }
-//
-// const userAgentApp = new UserAgentApplication(applicationConfig.clientID, applicationConfig.authority, (errorDesc, token, error, tokenType) => {
-//   console.log(errorDesc, token, error, tokenType)
-// });
+import { UserAgentApplication } from "msal";
 
-const applicationConfig = {b2cScopes: ""}
-const userAgentApp = {
-  getUser: () => {
-  }, loginRedirect: (s) => {
-    console.log(s)
-  }, logout: () => {
-  }
+const applicationConfig = {
+  clientID: "bacb8d3b-6ee0-4443-9bea-b54485a5a20d",
+  authority:
+    "https://login.microsoftonline.com/tfp/unihearti.onmicrosoft.com/B2C_1_tictactoe",
+  b2cScopes: ["https://unihearti.onmicrosoft.com/hello/demo.read"]
 };
+
+const userAgentApp = new UserAgentApplication(
+  applicationConfig.clientID,
+  applicationConfig.authority,
+  (errorDesc, token, error, tokenType) => {
+    console.log(errorDesc, token, error, tokenType);
+  }
+);
 
 export default class User {
   static getAgent() {
@@ -29,10 +25,10 @@ export default class User {
   }
 
   static login() {
-    userAgentApp.loginRedirect(applicationConfig.b2cScopes)
+    userAgentApp.loginRedirect(applicationConfig.b2cScopes);
   }
 
   static logout() {
-    userAgentApp.logout()
+    userAgentApp.logout();
   }
 }
