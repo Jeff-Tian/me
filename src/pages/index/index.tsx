1cbf877c54bc1013427473e2decaa46787798c24
Jeff Tian <jie.tian@unicareer.com>
2019-10-28 11:09:31 +0800

    feat: style ul li

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,15 +1,15 @@
-import { ComponentClass } from "react";
-import Taro, { Component, Config } from "@tarojs/taro";
-import { View } from "@tarojs/components";
-import { connect } from "@tarojs/redux";
-import { AtButton, AtNavBar } from "taro-ui";
-import "taro-ui/dist/style/index.scss"; // 引入组件样式 - 方式一
-import { login, logout, setUser, loggedIn } from "../../actions/login";
-import querystring from "querystring";
+import {ComponentClass} from 'react'
+import Taro, {Component, Config} from '@tarojs/taro'
+import {View} from '@tarojs/components'
+import {connect} from '@tarojs/redux'
+import {AtButton, AtNavBar} from 'taro-ui'
+import 'taro-ui/dist/style/index.scss' // 引入组件样式 - 方式一
+import {login, logout, setUser, loggedIn} from '../../actions/login'
+import querystring from 'querystring'
 
-import "./index.styl";
-import User from "../../services/user";
-import LoggedIn from "../../components/logged-in";
+import './index.styl'
+import User from '../../services/user'
+import LoggedIn from '../../components/logged-in'
 
 // #region 书写注意
 //
@@ -22,112 +22,114 @@ import LoggedIn from "../../components/logged-in";
 // #endregion
 
 type PageStateProps = {
-  index: { loading: false; user: null };
-};
-
-type PageDispatchProps = {
-  login: () => void;
-  logout: () => void;
-  setUser: (user) => void;
-  citiLogin: () => void;
-};
-
-type PageOwnProps = {};
-
-type PageState = {
-  popup: any;
-};
-
-type IProps = PageStateProps & PageDispatchProps & PageOwnProps;
-
-interface Index {
-  props: IProps;
+  index: {loading: false; user: null}
 }
 
-let popup: any = null;
+type PageDispatchProps = {
+  login: () => void
+  logout: () => void
+  setUser: (user) => void
+  citiLogin: () => void
+}
+
+type PageOwnProps = {}
+
+type PageState = {
+  popup: any
+}
+
+type IProps = PageStateProps & PageDispatchProps & PageOwnProps
+
+interface Index {
+  props: IProps
+}
+
+let popup: any = null
 
 function popupLogic() {
   try {
     // @ts-ignore
-    const link = popup.location.href;
+    const link = popup.location.href
   } catch (ex) {
-    popup.close();
-    window.alert("之前打开的窗口已关闭, 请重新点击并在新打开的窗口中重试。");
+    popup.close()
+    window.alert('之前打开的窗口已关闭, 请重新点击并在新打开的窗口中重试。')
   } finally {
     popup.postMessage(
-      "https://uniheart.herokuapp.com/passport/citi?redirect_uri=" +
-      encodeURIComponent(location.origin + process.env.publicPath + "pages/callback/citi"),
+      'https://uniheart.herokuapp.com/passport/citi?redirect_uri=' +
+        encodeURIComponent(
+          location.origin + process.env.publicPath + 'pages/callback/citi'
+        ),
       window.location.origin
-    );
+    )
   }
 }
 
 @connect(
-  ({ index }) => ({
-    index
+  ({index}) => ({
+    index,
   }),
   dispatch => ({
     login() {
-      dispatch(login());
-      User.login().then(user => dispatch(setUser(user)));
+      dispatch(login())
+      User.login().then(user => dispatch(setUser(user)))
     },
     logout() {
-      dispatch(logout());
+      dispatch(logout())
       Taro.setStorageSync('userInfo', null)
-      User.logout();
-      dispatch(setUser(null));
+      User.logout()
+      dispatch(setUser(null))
     },
     setUser(user) {
-      dispatch(setUser(user));
+      dispatch(setUser(user))
     },
     citiLogin() {
-      dispatch(login());
+      dispatch(login())
 
       const interval = setInterval(() => {
         if (popup.closed) {
           console.log('hello')
-          clearInterval(interval);
+          clearInterval(interval)
         }
-      }, 1000);
+      }, 1000)
 
       if (!popup || popup.closed) {
-        popup = window.open();
+        popup = window.open()
         popup.document.write(
-          "<html><head><title>第三方登录 我的个人中心</title></head><body><p>正在加载中, 请稍等" +
-          " ……</p><script>window.addEventListener('message', function (event) {\n" +
-          "    console.log(event.data);\n" +
-          "\n" +
-          "    if (event.data.indexOf('http://') === 0 || event.data.indexOf('https://') === 0 || event.data.indexOf('//') === 0) {\n" +
-          "        location.href = event.data;\n" +
-          "    }\n" +
-          "}, false);\n" +
-          "\n" +
-          "window.opener.postMessage('listenerLoaded', window.location.origin);</script></body></html>"
-        );
+          '<html><head><title>第三方登录 我的个人中心</title></head><body><p>正在加载中, 请稍等' +
+            " ……</p><script>window.addEventListener('message', function (event) {\n" +
+            '    console.log(event.data);\n' +
+            '\n' +
+            "    if (event.data.indexOf('http://') === 0 || event.data.indexOf('https://') === 0 || event.data.indexOf('//') === 0) {\n" +
+            '        location.href = event.data;\n' +
+            '    }\n' +
+            '}, false);\n' +
+            '\n' +
+            "window.opener.postMessage('listenerLoaded', window.location.origin);</script></body></html>"
+        )
 
         window.addEventListener(
-          "message",
-          async function (event) {
-            console.log("event = ", event);
+          'message',
+          async function(event) {
+            console.log('event = ', event)
             if (event.origin !== window.location.origin) {
-              return;
+              return
             }
 
             if (!event.data) {
               // Ignore the redirecting messages.
-              return;
+              return
             }
 
-            if (event.data === "listenerLoaded") {
-              return popupLogic();
+            if (event.data === 'listenerLoaded') {
+              return popupLogic()
             }
 
             if (
-              typeof event.data === "string" &&
-              event.data.indexOf("?") === 0
+              typeof event.data === 'string' &&
+              event.data.indexOf('?') === 0
             ) {
-              var tokenResult = querystring.parse(event.data.substr(1));
-              console.log(tokenResult);
+              var tokenResult = querystring.parse(event.data.substr(1))
+              console.log(tokenResult)
 
               if (tokenResult.token) {
                 dispatch(loggedIn(tokenResult.token))
@@ -136,7 +138,7 @@ function popupLogic() {
                   const userInfo = await Taro.request({
                     url: 'https://uniheart.pa-ca.me/jwt/user',
                     header: {
-                      'Authorization': 'Bearer ' + tokenResult.token
+                      Authorization: 'Bearer ' + tokenResult.token,
                     },
                     method: 'GET',
                   })
@@ -147,15 +149,15 @@ function popupLogic() {
                 }
               }
 
-              return (popup || event.source).close();
+              return (popup || event.source).close()
             }
           },
           false
-        );
+        )
       }
 
-      popupLogic();
-    }
+      popupLogic()
+    },
   })
 )
 class Index extends Component {
@@ -167,25 +169,25 @@ class Index extends Component {
    * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
    */
   config: Config = {
-    navigationBarTitleText: "我的个人中心"
-  };
+    navigationBarTitleText: '我的个人中心',
+  }
 
   componentWillReceiveProps(nextProps) {
-    console.log(this.props, nextProps);
+    console.log(this.props, nextProps)
   }
 
-  state: PageState = { popup: null };
+  state: PageState = {popup: null}
 
-  componentWillUnmount() { }
+  componentWillUnmount() {}
 
   componentDidShow() {
-    this.props.setUser(User.get() || Taro.getStorageSync('userInfo'));
+    this.props.setUser(User.get() || Taro.getStorageSync('userInfo'))
   }
 
-  componentDidHide() { }
+  componentDidHide() {}
 
   handleClick() {
-    console.log(arguments);
+    console.log(arguments)
   }
 
   render() {
@@ -214,29 +216,30 @@ class Index extends Component {
                 {Taro.getEnv() === Taro.ENV_TYPE.WEAPP && '微信授权登录'}
               </AtButton>
               <br />
-              {Taro.getEnv() === Taro.ENV_TYPE.WEB ?
+              {Taro.getEnv() === Taro.ENV_TYPE.WEB ? (
                 <AtButton
                   onClick={this.props.citiLogin.bind(this)}
                   loading={this.props.index.loading}
                 >
                   花旗账号登录
-              </AtButton> : null}
+                </AtButton>
+              ) : null}
             </View>
           ) : (
-              <View>
-                <LoggedIn user={this.props.index.user} />
-                <AtButton
-                  type="primary"
-                  onClick={this.props.logout}
-                  loading={this.props.index.loading}
-                >
-                  Log Out
+            <View>
+              <LoggedIn user={this.props.index.user} />
+              <AtButton
+                type="primary"
+                onClick={this.props.logout}
+                loading={this.props.index.loading}
+              >
+                Log Out
               </AtButton>
-              </View>
-            )}
+            </View>
+          )}
         </View>
       </View>
-    );
+    )
   }
 }
 
@@ -247,4 +250,4 @@ class Index extends Component {
 //
 // #endregion
 
-export default Index as ComponentClass<PageOwnProps, PageState>;
+export default Index as ComponentClass<PageOwnProps, PageState>
--- test/citi.spec.ts
+++ test/citi.spec.ts
@@ -1,68 +1,66 @@
-import puppeteer from "puppeteer";
-import path from "path";
-import GIFEncoder from "gifencoder";
-import pngFileStream from "png-file-stream";
-import fs from "fs";
+import puppeteer from 'puppeteer'
+import path from 'path'
+import GIFEncoder from 'gifencoder'
+import pngFileStream from 'png-file-stream'
+import fs from 'fs'
 
-const APP = "https://pa-ca.me/";
-let page;
-let browser;
-const width = 414;
-const height = 736;
+const APP = 'https://pa-ca.me/'
+let page
+let browser
+const width = 414
+const height = 736
 
-describe("OAuth Log In", () => {
+describe('OAuth Log In', () => {
   beforeAll(async () => {
     browser = await puppeteer.launch({
-      headless: "true" === process.env.ci,
-      args: [`--window-size=${width},${height}`]
-    });
-    page = await browser.newPage();
-    await page.setViewport({ width, height });
-  });
+      headless: 'true' === process.env.ci,
+      args: [`--window-size=${width},${height}`],
+    })
+    page = await browser.newPage()
+    await page.setViewport({width, height})
+  })
 
   afterAll(() => {
-    browser.close();
-  });
+    browser.close()
+  })
 
-  test("can navigate to Log In page", async () => {
+  test('can navigate to Log In page', async () => {
     async function testing() {
-      console.log("start testing...");
-      await page.goto(APP, { waitUntil: 'networkidle0' });
-      await page.waitForSelector("div.at-button__text");
-      console.log("testing done.");
-      const client = await page.target().createCDPSession();
+      console.log('start testing...')
+      await page.goto(APP, {waitUntil: 'networkidle0'})
+      await page.waitForSelector('div.at-button__text')
+      await page.tap('div.at-button.at-button--normal')
+      console.log('testing done.')
+      const client = await page.target().createCDPSession()
     }
 
     async function screenRecording() {
-      console.log("start recording...");
-      await new Promise(resolve => setTimeout(resolve, 10000));
-      process.stdout.write("taking screenshots: .");
-      const screenshotPromises = [];
+      console.log('start recording...')
+      await new Promise(resolve => setTimeout(resolve, 10000))
+      process.stdout.write('taking screenshots: .')
+      const screenshotPromises = []
       for (let i = 1; i < 15; ++i) {
-        const filename = path.resolve(
-          __dirname,
-          `T${new Date().getTime()}.png`
-        );
-        process.stdout.write(".");
-        screenshotPromises.push(page.screenshot({ path: filename, fullPage: true }));
+        const filename = path.resolve(__dirname, `T${new Date().getTime()}.png`)
+        process.stdout.write('.')
+        screenshotPromises.push(
+          page.screenshot({path: filename, fullPage: true})
+        )
 
-        await new Promise(resolve => setTimeout(resolve, 1000));
+        await new Promise(resolve => setTimeout(resolve, 1000))
       }
 
-      await new Promise(resolve => setTimeout(resolve, 1000));
-      await Promise.all(screenshotPromises);
+      await new Promise(resolve => setTimeout(resolve, 1000))
+      await Promise.all(screenshotPromises)
 
-      console.log("recording done, generating gif...");
-      const encoder = new GIFEncoder(width, height);
-      await pngFileStream(path.resolve(__dirname, "T*png"))
-        .pipe(
-          encoder.createWriteStream({ repeat: 0, deploy: 200, quality: 20 })
-        )
-        .pipe(fs.createWriteStream(path.resolve(__dirname, "screenshot.gif")));
+      console.log('recording done, generating gif...')
+      const encoder = new GIFEncoder(width, height)
+      await pngFileStream(path.resolve(__dirname, 'T*png'))
+        .pipe(encoder.createWriteStream({repeat: 0, deploy: 200, quality: 20}))
+        .pipe(fs.createWriteStream(path.resolve(__dirname, 'screenshot.gif')))
 
-      console.log("gif generated.");
+      console.log('gif generated.')
     }
 
-    await Promise.all([testing(), screenRecording()]);
-  });
-});
+    await Promise.all([testing(), screenRecording()])
+  })
+})
