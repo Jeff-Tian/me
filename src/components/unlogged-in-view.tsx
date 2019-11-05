bda6363329c26a6cb8318b79243f1ee24d087244
Jeff Tian <jie.tian@unicareer.com>
2019-11-05 21:05:35 +0800

    refactor: use functional component

--- src/components/unlogged-in-view.tsx
+++ src/components/unlogged-in-view.tsx
@@ -1,33 +1,35 @@
-import Taro from "@tarojs/taro";
-import { View } from "@tarojs/components";
-import { AtButton } from "taro-ui";
+import Taro from '@tarojs/taro'
+import {View} from '@tarojs/components'
+import {AtButton, AtDivider} from 'taro-ui'
 
 type UnLoggedInViewProps = {
-    login: () => void,
-    index: { loading: boolean },
-    citiLogin: () => void
+  login: () => void,
+  index: {loading: boolean},
+  citiLogin: () => void
 }
 
-export default class UnLoggedInView extends Taro.Component<UnLoggedInViewProps> {
-    render() {
-        return <View className="container-main">
-            <AtButton
-                type="primary"
-                onClick={this.props.login}
-                loading={this.props.index.loading}
-            >
-                {Taro.getEnv() === Taro.ENV_TYPE.WEB ? '微软 AD 账号' : ''}
-                {Taro.getEnv() === Taro.ENV_TYPE.WEAPP && '微信授权登录'}
-            </AtButton>
-            <br />
-            {Taro.getEnv() === Taro.ENV_TYPE.WEB ? (
-                <AtButton
-                    onClick={this.props.citiLogin.bind(this)}
-                    loading={this.props.index.loading}
-                >
-                    花旗账号登录
-                </AtButton>
-            ) : null}
-        </View>
-    }
+export default function UnLoggedInView(props: UnLoggedInViewProps) {
+  return (
+    <View className='container-main'>
+      <AtButton
+        type='primary'
+        onClick={props.login}
+        loading={props.index.loading}
+      >
+        {Taro.getEnv() === Taro.ENV_TYPE.WEB ? '微软 AD 账号' : ''}
+        {Taro.getEnv() === Taro.ENV_TYPE.WEAPP && '微信授权登录'}
+      </AtButton>
+
+      <AtDivider />
+
+      {Taro.getEnv() === Taro.ENV_TYPE.WEB ? (
+        <AtButton
+          onClick={props.citiLogin.bind(this)}
+          loading={props.index.loading}
+        >
+          花旗账号登录
+        </AtButton>
+      ) : null}
+    </View>
+  )
 }
